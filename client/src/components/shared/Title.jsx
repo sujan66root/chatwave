cf2d688498e73fe00b0783c5955c0891ed9837db
Sujan Gautam <sujan.rock45@gmail.com>
2024-05-24 21:52:09 -0400

    added some shared contents in the app

--- client/src/components/shared/Title.jsx
+++ client/src/components/shared/Title.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 const Title = ({
-    title = "Chat App",
-    description = "This is the Chat App called ChatWave",
+  title = "Chat App",
+  description = "this is the Chat App called Chattu",
 }) => {
-    return (
-        <Helmet>
-            <title>{title}</title>
-            <meta name="description" content={description} />
-        </Helmet>
-    );
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
 };
 
 export default Title;
