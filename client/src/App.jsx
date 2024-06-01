553951e9733a96db4dc7845f902e6dacad17e1b9
Sujan Gautam <sujan.rock45@gmail.com>
2024-05-31 23:58:15 -0400

    update and clean up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,80 @@
-import React, { lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ProtectedRoute from './components/auth/ProtectedRoute'
+import React, { Suspense, lazy, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { LayoutLoader } from "./components/layout/Loaders";
+import axios from "axios";
+import { server } from "./constants/config";
+import { useDispatch, useSelector } from "react-redux";
+import { userExists, userNotExists } from "./redux/reducers/auth";
+import { Toaster } from "react-hot-toast";
+import { SocketProvider } from "./socket";
 
-// @ts-ignore
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Chat = lazy(() => import("./pages/Chat"))
-const Group = lazy(() => import("./pages/Group"))
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
-let user = true;
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
+const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+const MessagesManagement = lazy(() =>
+  import("./pages/admin/MessageManagement")
+);
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <ProtectedRoute user={user} />
-          }
-        >
-          <Route path="/" element={<Home />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/groups" element={<Group />} />
-        </Route>
+  const { user, loader } = useSelector((state) => state.auth);
 
-        <Route
-          path="/login"
-          element={
-            <ProtectedRoute user={!user} redirect="/">
-              <Login />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get(`${server}/api/v1/user/me`, { withCredentials: true })
+      .then(({ data }) => dispatch(userExists(data.user)))
+      .catch((err) => dispatch(userNotExists()));
+  }, [dispatch]);
+
+  return loader ? (
+    <LayoutLoader />
+  ) : (
+    <BrowserRouter>
+      <Suspense fallback={<LayoutLoader />}>
+        <Routes>
+          <Route
+            element={
+              <SocketProvider>
+                <ProtectedRoute user={user} />
+              </SocketProvider>
+            }
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute user={!user} redirect="/">
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/users" element={<UserManagement />} />
+          <Route path="/admin/chats" element={<ChatManagement />} />
+          <Route path="/admin/messages" element={<MessagesManagement />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+
+      <Toaster position="bottom-center" />
     </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
